a096f42d7061e69e6b3f77cdbc4c58855c3240a4
Caleb Ashcraft <CALEBASHCRAFT@boisestate.edu>
2025-09-16 12:57:04 -0600

    Output Files

--- scripts/final/Double_Plot.R
+++ scripts/final/Double_Plot.R
@@ -1,5 +1,14 @@
-###Plot both Vector and Raster
+# Save TIFF file
+tiff("~/R Studio/inclass07-goshdangy/outputs/my_plot2.tif", 
+     width = 800, height = 800, res = 150)
+
 par(mfrow = c(1, 2))   # 1 row, 2 columns
 mycols <- colorRampPalette(c("blue", "white", "red"))(20)
-plot(st_geometry(cejst_shp))
+
+# Left: Vector
+plot(st_geometry(cejst_shp), main = "Vector")
+
+# Right: Raster
 plot(fire_prob, col = mycols, main = "Fire Risk")
+
+dev.off()  # closes the file
